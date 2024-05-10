2732efa31fff8ec46b5a150400b60d3168074bf6
wmh13 <165219928+wmh13@users.noreply.github.com>
2024-05-10 08:41:34 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # resources "tacos"
+  get("/tacos", :controller => "tacos", :action => "index")
+  # resources "dice"
+  get("/dice", :controller => "dice", :action => "index")
+  # resources "cards"
+  get("/cards", :controller => "cards", :action => "index")
+  # resources "bitcoins"
+  get("bitcoins", :controller => "bitcoins", :action => "index")
+  # resources "companies"
+  get("/companies", :controller => "companies", :action => "index")
+  # resources "contacts"
+  get("/contacts", :controller => "contacts", :action => "index")
+  # resources "posts"
+  get("/posts", :controller => "posts", :action => "index")
+end
